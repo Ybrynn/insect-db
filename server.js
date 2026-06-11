8376361470600e0260db281c17965aae96810435
Ybrynn <yulinglang211@outlook.com>
2026-06-11 15:48:02 +0800

    fix: 修复数据库回退路径为/data，添加dotenv支持

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+﻿require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const { initDb, query, queryOne, execute, logOperation, saveDb, getDb, DATA_DIR } = require('./lib/db');
